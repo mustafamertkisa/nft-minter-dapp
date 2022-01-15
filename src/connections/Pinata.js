fa4c26a76df35586cc8b6bcb897887896d03bb85
mustafamertkisa <m.mertkisa@gmail.com>
2022-01-15 19:31:51 +0300

    Pinata API keys updated

--- src/connections/Pinata.js
+++ src/connections/Pinata.js
@@ -1,9 +1,10 @@
 import { mintNft } from './Connect';
 
-const axios = require('axios');
+require('dotenv').config();
+const key = process.env.REACT_APP_PINATA_KEY;
+const secret = process.env.REACT_APP_PINATA_SECRET;
 
-const pinataApiKey = "74b2194646b33bc4dcf4";
-const pinataSecretApiKey = "d64289f47aee99c748a12ec1bc4b06ba58192869f4ee1b9ffb4c097f1303d7c1";
+const axios = require('axios');
 
 export let metadataKey = null;
 export const pinJSONToIPFS = (JSONBody) => {
@@ -11,8 +12,8 @@ export const pinJSONToIPFS = (JSONBody) => {
     return axios
         .post(url, JSONBody, {
             headers: {
-                pinata_api_key: pinataApiKey,
-                pinata_secret_api_key: pinataSecretApiKey
+                pinata_api_key: key,
+                pinata_secret_api_key: secret
             }
         })
         .then(function (response) {
